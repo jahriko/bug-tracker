348ac99612749ea148882781ad473ceb567ce12b
Jericho Opsima <jerichoopsima22@gmail.com>
2023-11-30 00:04:22 +0800

    improve validation in server action

--- actions/formAction.ts
+++ actions/formAction.ts
@@ -2,19 +2,38 @@
 
 import db from "@/lib/prisma"
 import { revalidatePath } from "next/cache";
+import { z } from "zod";
+
+const schema = z.object({
+	title: z.string().min(1).max(50),
+})
 
 export default async function createProject(formData: FormData) {
-	const title = formData.get('title')
+	const parse = schema.safeParse({
+		title: formData.get('title')
+	})
 
-	const project = await db.project.create({
-		data: {
-			title: title as string,
-		},
-	});
-
-	if (!project) {
+	if (!parse.success) {
 		return { error: "Something went wrong.", project: null }
 	}
 
-	revalidatePath('/')
+	const data = parse.data
+
+	try {
+		const project = await db.project.create({
+			data: {
+				title: data.title as string,
+			},
+		});
+
+		if (project) {
+			return { error: "Project already exists.", project: null }
+		}
+		
+		revalidatePath("/");
+		return { message: "Project created successfully.", project: project  }
+	} catch (error) {
+		return { message: "Something went wrong.", project: null}
+	}
+
 }
