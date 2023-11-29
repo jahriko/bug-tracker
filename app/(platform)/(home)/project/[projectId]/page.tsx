120c1872695ef84a9bd8e09bc2b41375946b8592
Jericho Opsima <jerichoopsima22@gmail.com>
2023-11-30 00:07:21 +0800

    remove mock object

--- app/(platform)/(home)/project/[projectId]/page.tsx
+++ app/(platform)/(home)/project/[projectId]/page.tsx
@@ -15,51 +15,6 @@ async function getTasks() {
 
 	return z.array(taskSchema).parse(tasks);
 }
-
-const invoices = [
-	{
-		invoice: "INV001",
-		paymentStatus: "Paid",
-		totalAmount: "$250.00",
-		paymentMethod: "Credit Card",
-	},
-	{
-		invoice: "INV002",
-		paymentStatus: "Pending",
-		totalAmount: "$150.00",
-		paymentMethod: "PayPal",
-	},
-	{
-		invoice: "INV003",
-		paymentStatus: "Unpaid",
-		totalAmount: "$350.00",
-		paymentMethod: "Bank Transfer",
-	},
-	{
-		invoice: "INV004",
-		paymentStatus: "Paid",
-		totalAmount: "$450.00",
-		paymentMethod: "Credit Card",
-	},
-	{
-		invoice: "INV005",
-		paymentStatus: "Paid",
-		totalAmount: "$550.00",
-		paymentMethod: "PayPal",
-	},
-	{
-		invoice: "INV006",
-		paymentStatus: "Pending",
-		totalAmount: "$200.00",
-		paymentMethod: "Bank Transfer",
-	},
-	{
-		invoice: "INV007",
-		paymentStatus: "Unpaid",
-		totalAmount: "$300.00",
-		paymentMethod: "Credit Card",
-	},
-];
  
 
 export default async function ProjectId({ params }: { params: { projectId: string } }) {
@@ -76,29 +31,8 @@ export default async function ProjectId({ params }: { params: { projectId: strin
 					</div>
 				</header>
 				<main>
-					<div className="mx-auto max-w-7xl sm:px-6 lg:px-8">
+					<div className="mx-auto lg:max-w-[1500px] sm:px-6 lg:px-8 max-w-full">
 						<DataTable data={tasks} columns={columns} />
-						{/* <Table>
-								<TableCaption>A list of your recent invoices.</TableCaption>
-								<TableHeader>
-									<TableRow>
-										<TableHead className="w-[100px]">Invoice</TableHead>
-										<TableHead>Status</TableHead>
-										<TableHead>Method</TableHead>
-										<TableHead className="text-right">Amount</TableHead>
-									</TableRow>
-								</TableHeader>
-								<TableBody>
-									{invoices.map((invoice) => (
-										<TableRow key={invoice.invoice}>
-											<TableCell className="font-medium">{invoice.invoice}</TableCell>
-											<TableCell>{invoice.paymentStatus}</TableCell>
-											<TableCell>{invoice.paymentMethod}</TableCell>
-											<TableCell className="text-right">{invoice.totalAmount}</TableCell>
-										</TableRow>
-									))}
-								</TableBody>
-							</Table> */}
 					</div>
 				</main>
 			</div>
