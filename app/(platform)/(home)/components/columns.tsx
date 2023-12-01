fbc04ade18df3003a211b5b953bf5fc2ad5c8e01
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-01 20:36:01 +0800

    remove unused component

--- app/(platform)/(home)/components/columns.tsx
+++ app/(platform)/(home)/components/columns.tsx
@@ -73,9 +73,6 @@ export const columns: ColumnDef<Task>[] = [
 		header: ({ column }) => <DataTableColumnHeader column={column} title="Title" />,
 		cell: ({ row }) => {
 			// const label = labels.find((label) => label.value === row.original.label)
-
-			
-
 			return (
 				<>
 					<Sheet>
@@ -107,76 +104,6 @@ export const columns: ColumnDef<Task>[] = [
 											</div>
 
 											<Separator className="my-10" />
-
-											{/* <ul role="list" className="mt-6 space-y-6">
-												{activity.map((activityItem, activityItemIdx) => (
-													<li key={activityItem.id} className="relative flex gap-x-4">
-														<div
-															className={classNames(
-																activityItemIdx === activity.length - 1
-																	? "h-6"
-																	: "-bottom-6",
-																"absolute left-0 top-0 flex w-6 justify-center"
-															)}
-														>
-															<div className="w-px bg-gray-200" />
-														</div>
-														{activityItem.type === "commented" ? (
-															<>
-																<img
-																	src={activityItem.person.imageUrl}
-																	alt=""
-																	className="relative mt-3 h-6 w-6 flex-none rounded-full bg-gray-50"
-																/>
-																<div className="flex-auto rounded-md p-3 ring-1 ring-inset ring-gray-200">
-																	<div className="flex justify-between gap-x-4">
-																		<div className="py-0.5 text-xs leading-5 text-gray-500">
-																			<span className="font-medium text-gray-900">
-																				{activityItem.person.name}
-																			</span>{" "}
-																			commented
-																		</div>
-																		<time
-																			dateTime={activityItem.dateTime}
-																			className="flex-none py-0.5 text-xs leading-5 text-gray-500"
-																		>
-																			{activityItem.date}
-																		</time>
-																	</div>
-																	<p className="text-sm leading-6 text-gray-500">
-																		{activityItem.comment}
-																	</p>
-																</div>
-															</>
-														) : (
-															<>
-																<div className="relative flex h-6 w-6 flex-none items-center justify-center bg-white">
-																	{activityItem.type === "paid" ? (
-																		<CheckCircleIcon
-																			className="h-6 w-6 text-indigo-600"
-																			aria-hidden="true"
-																		/>
-																	) : (
-																		<div className="h-1.5 w-1.5 rounded-full bg-gray-100 ring-1 ring-gray-300" />
-																	)}
-																</div>
-																<p className="flex-auto py-0.5 text-xs leading-5 text-gray-500">
-																	<span className="font-medium text-gray-900">
-																		{activityItem.person.name}
-																	</span>{" "}
-																	{activityItem.type} the invoice.
-																</p>
-																<time
-																	dateTime={activityItem.dateTime}
-																	className="flex-none py-0.5 text-xs leading-5 text-gray-500"
-																>
-																	{activityItem.date}
-																</time>
-															</>
-														)}
-													</li>
-												))}
-											</ul> */}
 
 											<div className="flow-root">
 												<ul role="list" className="-mb-8">
