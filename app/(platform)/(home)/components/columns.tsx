f84a5351b7d9fc6e51179dd4e9aca49a94521c07
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-01 20:34:13 +0800

    remove unused table columns

--- app/(platform)/(home)/components/columns.tsx
+++ app/(platform)/(home)/components/columns.tsx
@@ -68,37 +68,6 @@ function classNames(...classes) {
 
 
 export const columns: ColumnDef<Task>[] = [
-	// {
-	// 	id: "select",
-	// 	header: ({ table }) => (
-	// 		<Checkbox
-	// 			checked={
-	// 				table.getIsAllPageRowsSelected() ||
-	// 				(table.getIsSomePageRowsSelected() && "indeterminate")
-	// 			}
-	// 			onCheckedChange={(value: any) => table.toggleAllPageRowsSelected(!!value)}
-	// 			aria-label="Select all"
-	// 			className="translate-y-[2px]"
-	// 		/>
-	// 	),
-	// 	cell: ({ row }) => (
-	// 		<Checkbox
-	// 			checked={row.getIsSelected()}
-	// 			onCheckedChange={(value: any) => row.toggleSelected(!!value)}
-	// 			aria-label="Select row"
-	// 			className="translate-y-[2px]"
-	// 		/>
-	// 	),
-	// 	enableSorting: false,
-	// 	enableHiding: false,
-	// },
-	// {
-	// 	accessorKey: "id",
-	// 	header: ({ column }) => <DataTableColumnHeader column={column} title="Task" />,
-	// 	cell: ({ row }) => <div className="w-[80px]">{row.getValue("id")}</div>,
-	// 	enableSorting: false,
-	// 	enableHiding: false,
-	// },
 	{
 		accessorKey: "title",
 		header: ({ column }) => <DataTableColumnHeader column={column} title="Title" />,
@@ -469,35 +438,5 @@ export const columns: ColumnDef<Task>[] = [
 			return value.includes(row.getValue(id));
 		},
 	},
-	// {
-	//   accessorKey: "priority",
-	//   header: ({ column }) => (
-	//     <DataTableColumnHeader column={column} title="Priority" />
-	//   ),
-	//   cell: ({ row }) => {
-	//     const priority = priorities.find(
-	//       (priority) => priority.value === row.getValue("priority")
-	//     )
 
-	//     if (!priority) {
-	//       return null
-	//     }
-
-	//     return (
-	//       <div className="flex items-center">
-	//         {priority.icon && (
-	//           <priority.icon className="mr-2 h-4 w-4 text-muted-foreground" />
-	//         )}
-	//         <span>{priority.label}</span>
-	//       </div>
-	//     )
-	//   },
-	//   filterFn: (row, id, value) => {
-	//     return value.includes(row.getValue(id))
-	//   },
-	// },
-	// {
-	//   id: "actions",
-	//   cell: ({ row }) => <DataTableRowActions row={row} />,
-	// },
 ];
