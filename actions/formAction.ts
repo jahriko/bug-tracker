2e880a2dc58c7c93d430815a9540d9d9259624d6
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-03 21:29:36 +0800

    refactor: db named import

--- actions/formAction.ts
+++ actions/formAction.ts
@@ -1,6 +1,6 @@
 "use server"
 
-import db from "@/lib/prisma"
+import { db } from "@/lib/prisma"
 import { revalidatePath } from "next/cache";
 import { z } from "zod";
 
@@ -30,7 +30,7 @@ export default async function createProject(formData: FormData) {
 			return { error: "Project already exists.", project: null }
 		}
 		
-		revalidatePath("/");
+		revalidatePath("/projects");
 		return { message: "Project created successfully.", project: project  }
 	} catch (error) {
 		return { message: "Something went wrong.", project: null}
