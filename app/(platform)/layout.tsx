322b252eb88566e9b9424e884494e25c26e1fb0e
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 16:20:29 +0800

    Remove ClerkProvider import from layout.tsx

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,7 +1,6 @@
 import { Toaster } from "@/components/ui/toaster"
 import NextAuthSessionProvider from "@/lib/nextauth-provider"
 import TanstackProvider from "@/lib/tanstack-provider"
-import { ClerkProvider } from "@clerk/nextjs"
 
 export default function PlatformLayout({
   children,
