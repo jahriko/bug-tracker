0047d7d65116551f150aaa12833de04cb34f633a
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 16:18:20 +0800

    Add authentication check in DashboardLayout

--- app/(platform)/(home)/layout.tsx
+++ app/(platform)/(home)/layout.tsx
@@ -1,16 +1,24 @@
 import { db } from "@/lib/prisma"
 import Sidebar from "./_components/sidebar"
+import { auth } from "@/lib/auth"
+import { redirect } from "next/navigation"
 
 async function getProjects() {
 	return db.project.findMany()
 }
 
 export default async function DashboardLayout({
-	children
-}: { children: React.ReactNode }) {
-	return (
-		<>
-			<Sidebar projects={await getProjects()} />
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  const session = await auth()
+
+  if (!session || !session.user?.email) {
+    redirect("/login")
+  }
+
+  const profile = session?.user
 
 			<main className="py-10 lg:pl-64 bg-white">
 				<div className="px-4 sm:px-6 lg:px-8">{children}</div>
