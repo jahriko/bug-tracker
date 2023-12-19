b62a4ca9ac0c41e4d7317eaace299d9f8741660e
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 16:02:16 +0800

    replace logo to profile dropdown

--- app/(platform)/(home)/_components/sidebar.tsx
+++ app/(platform)/(home)/_components/sidebar.tsx
@@ -181,83 +181,39 @@ export default function Sidebar({ projects, profile }: SidebarProps) {
 				</Dialog>
 			</Transition.Root>
 
-			{/* Static sidebar for desktop */}
-			<div className="hidden lg:fixed lg:inset-y-0 lg:z-50 lg:flex lg:w-64 lg:flex-col ">
-				{/* Sidebar component, swap this element with another sidebar if you like */}
-				<div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-white px-6">
-					<div className="flex h-16 shrink-0 items-center">
-						<img
-							className="h-8 w-auto"
-							src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-							alt="Your Company"
-						/>
-					</div>
-					<nav className="flex flex-1 flex-col">
-						<ul className="flex flex-1 flex-col gap-y-7">
-							<li>
-								<ul className="-mx-2 space-y-1">
-									{navigation.map((item) => (
-										<li key={item.name}>
-											<Link
-												href={item.href}
-												className={classNames(
-													path === item.href
-														? "bg-gray-50 text-indigo-600"
-														: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-													"group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-												)}
-											>
-												<item.icon
-													className={classNames(
-														path === item.href
-															? "text-indigo-600"
-															: "text-gray-400 group-hover:text-indigo-600",
-														"h-6 w-6 shrink-0"
-													)}
-													aria-hidden="true"
-												/>
-												{item.name}
-											</Link>
-										</li>
-									))}
-								</ul>
-							</li>
-							<li>
-								<div className="text-xs font-semibold leading-6 text-gray-400">
-									Projects
-								</div>
-								<ul className="-mx-2 mt-2 space-y-1">
-									{projects?.map((project: any) => (
-										<li key={project.id}>
-											<Link
-												href={`projects/${project.id}`}
-												className={classNames(
-													project.current
-														? "bg-gray-50 text-indigo-600"
-														: "text-gray-700 hover:text-indigo-600 hover:bg-gray-50",
-													"group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold"
-												)}
-											>
-												<span
-													className={classNames(
-														project.current
-															? "text-indigo-600 border-indigo-600"
-															: "text-gray-400 border-gray-200 group-hover:border-indigo-600 group-hover:text-indigo-600",
-														"flex h-6 w-6 shrink-0 items-center justify-center rounded-lg border text-[0.625rem] font-medium bg-white"
-													)}
-												>
-													{project.title.charAt(0)}
-												</span>
-												<span className="truncate">{project.title}</span>
-											</Link>
-										</li>
-									))}
-								</ul>
-							</li>
-						</ul>
-					</nav>
-				</div>
-			</div>
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <div className="mt-4 flex shrink-0 cursor-pointer items-center">
+                <div className="flex items-center space-x-4">
+                  <Avatar>
+                    <AvatarImage src={profile.image} />
+                    <AvatarFallback>
+                      {profile.name?.charAt(0).toUpperCase()}
+                    </AvatarFallback>
+                  </Avatar>
+                  <div>
+                    <p className="text-sm font-medium leading-none">
+                      {profile.name}
+                    </p>
+                    <p className="text-sm text-muted-foreground">
+                      {profile.email}
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56" align="end" forceMount>
+              <DropdownMenuGroup>
+                <DropdownMenuItem>Profile</DropdownMenuItem>
+              </DropdownMenuGroup>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                onClick={() => signOut({ callbackUrl: "/login" })}
+              >
+                Log out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
 
 			<div className="sticky top-0 z-40 flex items-center gap-x-6 bg-white px-4 py-4 shadow-sm sm:px-6 lg:hidden ">
 				<button
