e0670a2aa5a57aa40c4ca8ca308506e5a2f7de7b
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 15:59:17 +0800

    remove sample data

--- app/(platform)/(home)/_components/columns.tsx
+++ app/(platform)/(home)/_components/columns.tsx
@@ -12,7 +12,6 @@ import {
 	XMarkIcon as XMarkIconMini
 } from "@heroicons/react/20/solid"
 
-import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
 import {
 	Sheet,
@@ -20,54 +19,7 @@ import {
 	SheetTitle,
 	SheetTrigger
 } from "@/components/ui/sheet"
-import { Textarea } from "@/components/ui/textarea"
 import { StopwatchIcon } from "@radix-ui/react-icons"
-import Link from "next/link"
-import { Fragment } from "react"
-
-const activity = [
-	{
-		id: 1,
-		type: "comment",
-		person: { name: "Eduardo Benz", href: "#" },
-		imageUrl:
-			"https://images.unsplash.com/photo-1520785643438-5bf77931f493?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-		comment:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. ",
-		date: "6d ago"
-	},
-	{
-		id: 2,
-		type: "assignment",
-		person: { name: "Hilary Mahy", href: "#" },
-		assigned: { name: "Kristin Watson", href: "#" },
-		date: "2d ago"
-	},
-	{
-		id: 3,
-		type: "tags",
-		person: { name: "Hilary Mahy", href: "#" },
-		tags: [
-			{ name: "Bug", href: "#", color: "fill-red-500" },
-			{ name: "Accessibility", href: "#", color: "fill-indigo-500" }
-		],
-		date: "6h ago"
-	},
-	{
-		id: 4,
-		type: "comment",
-		person: { name: "Jason Meyers", href: "#" },
-		imageUrl:
-			"https://images.unsplash.com/photo-1531427186611-ecfd6d936c79?ixlib=rb-=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=8&w=256&h=256&q=80",
-		comment:
-			"Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tincidunt nunc ipsum tempor purus vitae id. Morbi in vestibulum nec varius. Et diam cursus quis sed purus nam. Scelerisque amet elit non sit ut tincidunt condimentum. Nisl ultrices eu venenatis diam.",
-		date: "2h ago"
-	}
-]
-
-function classNames(...classes) {
-	return classes.filter(Boolean).join(" ")
-}
 
 export const columns: ColumnDef<Task>[] = [
 	{
