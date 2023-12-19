4b53c206288ca1d74c896b4aca90e1cf56946a82
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 09:08:00 +0800

    extract and rename projectSchema

--- actions/formAction.ts
+++ actions/formAction.ts
@@ -1,6 +1,11 @@
 "use server"
 
-import { db } from "@/lib/prisma"
+import {
+  IssueSchema,
+  LoginSchema,
+  ProjectSchema,
+  RegisterSchema
+} from "@/types"
 import { revalidatePath } from "next/cache"
 import { z } from "zod"
 
@@ -8,31 +13,31 @@ const schema = z.object({
 	title: z.string().min(1).max(50)
 })
 
-export default async function createProject(formData: FormData) {
-	const parse = schema.safeParse({
-		title: formData.get("title")
-	})
+export async function createProject(formData: FormData) {
+  const parse = ProjectSchema.safeParse({
+    title: formData.get("title")
+  })
 
-	if (!parse.success) {
-		return { error: "Something went wrong.", project: null }
-	}
+  if (!parse.success) {
+    return { error: "Something went wrong.", project: null }
+  }
 
-	const data = parse.data
+  const data = parse.data
 
-	try {
-		const project = await db.project.create({
-			data: {
-				title: data.title as string
-			}
-		})
+  try {
+    const project = await prisma.project.create({
+      data: {
+        title: data.title as string
+      }
+    })
 
-		if (project) {
-			return { error: "Project already exists.", project: null }
-		}
+    if (project) {
+      return { error: "Project already exists.", project: null }
+    }
 
-		revalidatePath("/projects")
-		return { message: "Project created successfully.", project: project }
-	} catch (error) {
-		return { message: "Something went wrong.", project: null }
-	}
+    revalidatePath("/projects")
+    return { message: "Project created successfully.", project: project }
+  } catch (error) {
+    return { message: "Something went wrong.", project: null }
+  }
 }
