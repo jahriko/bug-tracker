9953b68bb8ca3736aa41ceb8131e1f73ce3da94d
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 16:17:02 +0800

    Add TanstackProvider to PlatformLayout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -4,9 +4,11 @@ import TanstackProvider from "@/lib/tanstack-provider"
 import { ClerkProvider } from "@clerk/nextjs"
 
 export default function PlatformLayout({
+    <TanstackProvider>
       <NextAuthSessionProvider>
         {children}
         <Toaster />
       </NextAuthSessionProvider>
+    </TanstackProvider>
   )
 }
