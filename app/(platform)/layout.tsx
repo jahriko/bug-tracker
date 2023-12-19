70da373669187298f75d2fb30ae883208bc154f4
Jericho Opsima <jerichoopsima22@gmail.com>
2023-12-19 16:16:43 +0800

    Add Toaster component to PlatformLayout

--- app/(platform)/layout.tsx
+++ app/(platform)/layout.tsx
@@ -1,3 +1,4 @@
+import { Toaster } from "@/components/ui/toaster"
 import NextAuthSessionProvider from "@/lib/nextauth-provider"
 import TanstackProvider from "@/lib/tanstack-provider"
 import { ClerkProvider } from "@clerk/nextjs"
@@ -5,6 +6,7 @@ import { ClerkProvider } from "@clerk/nextjs"
 export default function PlatformLayout({
       <NextAuthSessionProvider>
         {children}
+        <Toaster />
       </NextAuthSessionProvider>
   )
 }
