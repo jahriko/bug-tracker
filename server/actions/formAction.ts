b2f2774f82535650ad58ee452f3890c244be8158
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-11 23:52:16 +0800

    Add formAction.ts file for user registration

---
+++ server/actions/formAction.ts
@@ -0,0 +1,36 @@
+"use server"
+
+import { hash } from "bcrypt"
+import { revalidatePath } from "next/cache"
+import { auth } from "@/lib/auth"
+import prisma from "@/lib/prisma"
+import { ProjectSchema, RegisterSchema } from "@/types"
+
+export async function createUser(data: RegisterSchema) {
+  try {
+    const { name, email, password } = data
+
+    const userExists = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+
+    if (userExists) {
+      return { error: "User already exists" }
+    }
+
+    await prisma.user.create({
+      data: {
+        name,
+        email,
+        hashedPassword: await hash(password, 13),
+      },
+    })
+
+    revalidatePath("/login")
+  } catch (error) {
+    console.error(error)
+  }
+}
+
