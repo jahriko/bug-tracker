a6fdfab8915a793cf3678333bf4440895eaf12f4
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-11 23:53:30 +0800

    Refactor login form component

--- app/(platform)/(auth)/_components/login-form.tsx
+++ app/(platform)/(auth)/_components/login-form.tsx
@@ -1,17 +1,17 @@
 "use client"
 
-import { Icons } from "@/components/icons"
-import { Button } from "@/components/ui/button"
-import { Input } from "@/components/ui/input"
-import { Label } from "@/components/ui/label"
-import { useToast } from "@/components/ui/use-toast"
-import { type LoginSchema as LoginSchemaType, LoginSchema } from "@/types"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { signIn } from "next-auth/react"
 import Link from "next/link"
 import { useRouter } from "next/navigation"
 import { useState } from "react"
 import { useForm } from "react-hook-form"
+import { Icons } from "@/components/icons"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import { useToast } from "@/components/ui/use-toast"
+import { type LoginSchema as LoginSchemaType, LoginSchema } from "@/types"
 
 const LoginForm = () => {
   const {
@@ -25,7 +25,7 @@ const LoginForm = () => {
   const { toast } = useToast()
   const router = useRouter()
 
-  const onSubmit = async (data: LoginSchema) => {
+  async function onSubmit(data: LoginSchema) {
     setIsLoading(true)
 
     const login = await signIn("credentials", {
@@ -52,89 +52,80 @@ const LoginForm = () => {
   }
 
   return (
-    <>
-      <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
-          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
-            Sign in to your account
-          </h2>
-        </div>
-
-        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-          <form onSubmit={handleSubmit(onSubmit)} method="POST">
-            <div>
-              <Label
-                htmlFor="email"
-                className="block text-sm font-medium leading-6 text-gray-900"
-              >
-                Email address
-              </Label>
-              <div className="mt-2">
-                <Input
-                  id="email"
-                  type="email"
-                  autoComplete="email"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  disabled={isLoading}
-                  {...register("email")}
-                />
-                {errors?.email && (
-                  <p className="px-1 text-xs text-red-600">
-                    {errors.email.message}
-                  </p>
-                )}
-              </div>
-            </div>
-
-            <div>
-              <div className="flex items-center justify-between">
-                <Label
-                  htmlFor="password"
-                  className="block text-sm font-medium leading-6  text-gray-900"
-                >
-                  Password
-                </Label>
-              </div>
-              <div className="mt-2">
-                <Input
-                  id="password"
-                  type="password"
-                  autoComplete="current-password"
-                  required
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-                  disabled={isLoading}
-                  {...register("password")}
-                />
-              </div>
-            </div>
-
-            <div>
-              <Button
-                type="submit"
-                className="mt-6 w-full"
-                disabled={isLoading}
-              >
-                {isLoading && (
-                  <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
-                )}
-                Sign in
-              </Button>
-            </div>
-          </form>
-
-          <p className="text-md mt-5 text-center text-gray-600">
-            Need an account?
-            <Link
-              className="ml-1 text-blue-500 hover:underline"
-              href="/register"
-            >
-              Create one
-            </Link>
-          </p>
-        </div>
+    <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+      <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+        <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
       </div>
-    </>
+
+      <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+        <form method="POST" onSubmit={handleSubmit(onSubmit)}>
+          <div>
+            <Label
+              className="block text-sm font-medium leading-6 text-gray-900"
+              htmlFor="email"
+            >
+              Email address
+            </Label>
+            <div className="mt-2">
+              <Input
+                autoComplete="email"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                disabled={isLoading}
+                id="email"
+                required
+                type="email"
+                {...register("email")}
+              />
+              {errors.email ? (
+                <p className="px-1 text-xs text-red-600">
+                  {errors.email.message}
+                </p>
+              ) : null}
+            </div>
+          </div>
+
+          <div>
+            <div className="flex items-center justify-between">
+              <Label
+                className="block text-sm font-medium leading-6  text-gray-900"
+                htmlFor="password"
+              >
+                Password
+              </Label>
+            </div>
+            <div className="mt-2">
+              <Input
+                autoComplete="current-password"
+                className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                disabled={isLoading}
+                id="password"
+                required
+                type="password"
+                {...register("password")}
+              />
+            </div>
+          </div>
+
+          <div>
+            <Button className="mt-6 w-full" disabled={isLoading} type="submit">
+              {isLoading ? (
+                <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
+              ) : null}
+              Sign in
+            </Button>
+          </div>
+        </form>
+
+        <p className="text-md mt-5 text-center text-gray-600">
+          Need an account?
+          <Link className="ml-1 text-blue-500 hover:underline" href="/register">
+            Create one
+          </Link>
+        </p>
+      </div>
+    </div>
   )
 }
 
