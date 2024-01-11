befd24e5f3a4bf62c0aad4170738a28aa1211471
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-11 23:49:21 +0800

    Refactor TanstackProvider to use useState for queryClient

--- lib/tanstack-provider.tsx
+++ lib/tanstack-provider.tsx
@@ -1,13 +1,24 @@
 "use client"
 
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
-
-const client = new QueryClient()
+import { useState } from "react"
 
 export default function TanstackProvider({
   children,
 }: {
   children: React.ReactNode
 }) {
-  return <QueryClientProvider client={client}>{children}</QueryClientProvider>
+  const [queryClient] = useState(
+    () =>
+      new QueryClient({
+        defaultOptions: {
+          queries: {
+            staleTime: 60 * 1000,
+          },
+        },
+      }),
+  )
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  )
 }
