815eb5ac74cd1c13364edcbcbf125f8a19597345
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-11 23:47:55 +0800

    Add comment server action

---
+++ server/actions/comment.ts
@@ -0,0 +1,42 @@
+"use server"
+
+import { z } from "zod"
+import { revalidatePath } from "next/dist/server/web/spec-extension/revalidate-path"
+import prisma from "@/lib/prisma"
+import { getCurrentUser } from "@/lib/get-current-user"
+
+const FormSchema = z.object({
+  comment: z.string(),
+})
+
+export async function createComment(
+  issueId: number,
+  data: z.infer<typeof FormSchema>,
+) {
+  const user = await getCurrentUser()
+
+  const { comment } = FormSchema.parse(data)
+
+  try {
+    await prisma.comment.create({
+      data: {
+        text: comment,
+        userId: user.userId,
+        issueId,
+      },
+    })
+
+    revalidatePath(`/issue/${issueId}`)
+
+    return {
+      code: "success",
+      message: "Comment created successfully",
+    }
+  } catch (error) {
+    console.log(error)
+    return {
+      code: "error",
+      message: "Error creating comment",
+    }
+  }
+}
