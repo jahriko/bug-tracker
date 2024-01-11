7a87e52c3e7ecea27d5858a37f390c212d5dee17
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-12 00:01:06 +0800

    Refactor Inbox page to cache issues

--- app/(platform)/(home)/inbox/page.tsx
+++ app/(platform)/(home)/inbox/page.tsx
@@ -1,42 +1,36 @@
-import { promises as fs } from "fs"
-import path from "path"
-
-import { z } from "zod"
+// eslint-disable-next-line camelcase
+import { unstable_cache } from "next/cache"
 import { columns } from "../_components/columns"
 import { DataTable } from "../_components/data-table"
-import { taskSchema } from "../_data/schema"
-import { auth } from "@/lib/auth"
 
-async function getTasks() {
-  const data = await fs.readFile(
-    path.join(process.cwd(), "app/(platform)/(home)/_data/tasks.json"),
-  )
-
-  const tasks = JSON.parse(data.toString())
-
-  return z.array(taskSchema).parse(tasks)
-}
-
-export default async function Inbox() {
-  const tasks = await getTasks()
-  const session = await auth()
-
-  if (!session) {
-    return {
-      redirect: {
-        destination: "/login",
+const getCachedIssueList = unstable_cache(async () => {
+  const issueList = await prisma?.issue.findMany({
+    select: {
+      id: true,
+      title: true,
+      status: true,
+      label: true,
+      priority: true,
+      project: {
+        select: {
+          title: true,
+        },
       },
-    }
-  }
+    },
+  })
+  return issueList
+}, ["issue-list"])
+
+export default async function InboxPage() {
+  const issueList = await getCachedIssueList()
+
   return (
-    <>
-      <div className="py-6">
-        <main>
-          <div className="mx-auto max-w-full sm:px-6 lg:max-w-[1300px] lg:px-8">
-            <DataTable data={tasks} columns={columns} />
-          </div>
-        </main>
-      </div>
-    </>
+    <div className="py-6">
+      <main>
+        <div>
+          <DataTable columns={columns} data={issueList} />
+        </div>
+      </main>
+    </div>
   )
 }
