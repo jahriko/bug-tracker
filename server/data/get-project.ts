3bda178a2c6646a49f67546cbe5f4055bff77bbd
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-27 10:34:10 +0800

    Add get project action

---
+++ server/data/get-project.ts
@@ -0,0 +1,14 @@
+import { Prisma } from "@prisma/client"
+import prisma from "@/lib/prisma"
+
+export async function getProjects() {
+  const projects = await prisma.project.findMany({
+    select: {
+      id: true,
+      title: true,
+    },
+  })
+
+  return projects
+}
+export type ProjectsData = Prisma.PromiseReturnType<typeof getProjects>
