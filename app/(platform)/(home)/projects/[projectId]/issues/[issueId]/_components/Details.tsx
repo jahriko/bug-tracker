812a49f1fd6de17be85bedcf020882a58243c62a
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-27 23:46:09 +0800

    extract prioritybox component from details.tsx

--- app/(platform)/(home)/projects/[projectId]/issues/[issueId]/_components/Details.tsx
+++ app/(platform)/(home)/projects/[projectId]/issues/[issueId]/_components/Details.tsx
@@ -73,91 +73,9 @@ export function LabelBox({ issueLabels }: { issueLabels: IssueLabel[] }) {
   return renderLabels
 }
 
-const priorities = [
-  {
-    value: "no priority",
-    label: "No priority",
-    icon: MinusCircle,
-  },
-  {
-    value: "low",
-    label: "Low",
-    icon: SignalLow,
-  },
-  {
-    value: "medium",
-    label: "Medium",
-    icon: SignalMedium,
-  },
-  {
-    value: "high",
-    label: "High",
-    icon: SignalHigh,
-  },
-]
 
-export function PriorityBox({ priority }: { priority: string }) {
-  const [selected, setSelected] = useState<Status | null>({
-    value: "no priority",
-    label: "No priority",
-    icon: MinusCircle,
-  })
 
-  function PriorityIcon({ priority }: { priority: string | undefined }) {
-    if (priority === "no priority") {
-      return <MinusCircle className="mr-2 size-4" />
-    } else if (priority === "low") {
-      return <SignalLow className="mr-2 size-4" />
-    } else if (priority === "medium") {
-      return <SignalMedium className="mr-2 size-4" />
-    } else if (priority === "high") {
-      return <SignalHigh className="mr-2 size-4" />
-    }
-  }
 
-  return (
-    <Popover>
-      <PopoverTrigger asChild>
-        <Button className="-ml-2.5 w-[10rem] justify-start" variant="ghost">
-          <PriorityIcon priority={priority} />
-          <span className="capitalize">{priority || "No priority"}</span>
-        </Button>
-      </PopoverTrigger>
-      <PopoverContent align="start" className="p-0" side="bottom">
-        <Command>
-          <CommandInput placeholder="Change priority" />
-          <CommandList>
-            <CommandEmpty>No results found.</CommandEmpty>
-            <CommandGroup>
-              {priorities.map((priority) => (
-                <CommandItem
-                  key={priority.value}
-                  onSelect={(value) => {
-                    setSelected(
-                      priorities.find((priority) => priority.value === value) ??
-                        null,
-                    )
-                  }}
-                  value={priority.value}
-                >
-                  <priority.icon
-                    className={cn(
-                      "mr-2 h-4 w-4",
-                      priority.value === selected?.value
-                        ? "opacity-100"
-                        : "opacity-40",
-                    )}
-                  />
-                  <span>{priority.label}</span>
-                </CommandItem>
-              ))}
-            </CommandGroup>
-          </CommandList>
-        </Command>
-      </PopoverContent>
-    </Popover>
-  )
-}
 
 export function AssigneeBox({
   assignees,
