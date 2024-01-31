16f2bec8a060945c59760577d9a6b66db021164e
Jericho Opsima <jerichoopsima22@gmail.com>
2024-01-31 23:12:57 +0800

    Change modal to a page when creating an issue

--- app/(platform)/(home)/_components/Sidebar/SidebarMobileHeader.tsx
+++ app/(platform)/(home)/_components/Sidebar/SidebarMobileHeader.tsx
@@ -3,8 +3,16 @@
 /* eslint-disable @next/next/no-img-element */
 import { Bars3Icon } from "@heroicons/react/24/outline"
 import { useSidebar } from "./Sidebar"
+import ProfileDropdown from "@/app/(platform)/(home)/_components/Sidebar/SidebarProfileDropdown";
 
-export default function MobileHeader() {
+export default function MobileHeader({
+  profile,
+}: {
+  profile: {
+    name: string | undefined
+    email: string | undefined
+    image: string | undefined
+  }}) {
   const { setSidebarOpen } = useSidebar()
   return (
     <div className="sticky top-0 z-40 flex items-center gap-x-6 border-b border-gray-200 bg-white p-4">
@@ -22,14 +30,7 @@ export default function MobileHeader() {
         Dashboard
       </div>
 
-      <a href="/">
-        <span className="sr-only">Your profile</span>
-        <img
-          alt=""
-          className="h-8 w-8 rounded-full bg-gray-50"
-          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-        />
-      </a>
+      <ProfileDropdown profile={profile} />
     </div>
   )
 }
