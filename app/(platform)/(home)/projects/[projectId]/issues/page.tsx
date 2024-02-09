9cad509a6dbde1cc41ebc2b2f4223feec9fb2e5a
Jericho Opsima <jerichoopsima22@gmail.com>
2024-02-10 07:31:10 +0800

    Remove unnecessary code

--- app/(platform)/(home)/projects/[projectId]/issues/page.tsx
+++ app/(platform)/(home)/projects/[projectId]/issues/page.tsx
@@ -1,6 +1,3 @@
-import { promises as fs } from "fs"
-import path from "path"
-import { z } from "zod"
 import prisma from "@/lib/prisma"
 import { columns } from "@/components/DataTable/columns"
 import { DataTable } from "@/components/DataTable/data-table"
@@ -26,9 +23,7 @@ export default async function Issues({
   return (
     <div className="py-6">
       <main>
-        <div>
-          <DataTable columns={columns} data={issues} />
-        </div>
+        <DataTable columns={columns} data={issues} />
       </main>
     </div>
   )
