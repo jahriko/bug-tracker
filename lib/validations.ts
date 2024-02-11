f62989f942fe12656ba06b2932ce97e3b3a5a01d
Jericho Opsima <jerichoopsima22@gmail.com>
2024-02-11 21:50:58 +0800

    Move validation logic to separate file

--- lib/validations.ts
+++ lib/validations.ts
@@ -13,3 +13,36 @@ export const IssueSchema = z.object({
 })
 
 export type IssueSchema = z.infer<typeof IssueSchema>
+
+export const ProjectSchema = z.object({
+  title: z.string().min(1).max(50),
+})
+
+export type ProjectSchema = z.infer<typeof ProjectSchema>
+
+export const RegisterSchema = z
+  .object({
+    email: z.string().min(1, "Email is required!").email("Invalid email!"),
+    name: z.string().min(2, "Name is required!"),
+    password: z
+      .string()
+      .min(1, "Password is required!")
+      .min(8, "Password must have than 8 characters!"),
+    confirmPassword: z.string().min(1, "Password confirmation is required!"),
+  })
+  .refine((data) => data.password === data.confirmPassword, {
+    path: ["confirmPassword"],
+    message: "Password do not match!",
+  })
+
+export type RegisterSchema = z.infer<typeof RegisterSchema>
+
+export const LoginSchema = z.object({
+  email: z.string().email("Invalid email!"),
+  password: z
+    .string()
+    .min(1, "Password is required!")
+    .min(8, "Password must have than 8 characters!"),
+})
+
+export type LoginSchema = z.infer<typeof LoginSchema>
