eae35cc1c2f149d7a4b32fa9f6bfb85ea84ed7cf
Jericho Opsima <jerichoopsima22@gmail.com>
2024-06-19 12:38:58 +0800

    Add authenticated action middleware

--- lib/safe-action.ts
+++ lib/safe-action.ts
@@ -1,3 +1,18 @@
+import { auth } from "@/auth"
 import { createSafeActionClient } from "next-safe-action"
 
 export const actionClient = createSafeActionClient()
+
+export const authActionClient = actionClient.use(async ({ next }) => {
+  const session = await auth()
+
+  if (!session?.user.userId) {
+    throw new Error("User not found")
+  }
+
+  return next({
+    ctx: {
+      userId: session.user.userId,
+    },
+  })
+})
