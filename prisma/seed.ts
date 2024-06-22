8577e607c46d7791f8ce6cde01c04a34f2089b7c
Jericho Opsima <jerichoopsima22@gmail.com>
2024-06-22 11:41:01 +0800

    Update seed

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,23 +1,18 @@
+import { db } from "@/lib/extensions/enhanced"
 import prisma from "@/lib/prisma"
 import { faker } from "@faker-js/faker"
+import { enhance } from "@zenstackhq/runtime"
 import { hash } from "bcrypt"
-import { cookies } from "next/headers"
 
 async function main() {
-  console.log("Deleting previous seed...")
   await prisma.user.deleteMany()
+  await prisma.label.deleteMany()
   await prisma.workspace.deleteMany()
-  await prisma.project.deleteMany()
 
-  //Delete browser cookies
-  console.log("Deleting cookies...")
-  cookies()
-    .getAll()
-    .forEach((cookie) => {
-      cookies().delete(cookie.name)
-    })
+  // const db = enhance(prisma, {}, { logPrismaQuery: true })
 
-  await prisma.label.createMany({
+  // Create labels
+  const labels = await prisma.label.createManyAndReturn({
     data: [
       { name: "Bug", color: "red" },
       { name: "Feature", color: "green" },
@@ -29,67 +24,260 @@ async function main() {
     ],
   })
 
-  for (let i = 0; i < 5; i++) {
+  const labelIds = labels.map((label) => label.id)
+
+  // Create users
+  // let users
+  // for (let i = 0; i < 10; i++) {
+  //   const firstName = faker.person.firstName()
+  //   const lastName = faker.person.lastName()
+  //   const hashedPassword = await hash("12345678", 13)
+  //   users = await prisma.user.create({
+  //     data: {
+  //       image: faker.image.avatar(),
+  //       name: `${firstName} ${lastName}`,
+  //       email: faker.internet.exampleEmail({ firstName, lastName }),
+  //       hashedPassword,
+  //     },
+  //   })
+  // }
+
+  let userData = Array.from({ length: 20 }, async () => ({
+    image: faker.image.avatar(),
+    name: `${faker.person.firstName()} ${faker.person.lastName()}`,
+    email: faker.internet.exampleEmail(),
+    hashedPassword: await hash("12345678", 13),
+  }))
+
+  let users = await prisma.user.createManyAndReturn({
+    data: await Promise.all(userData),
+    skipDuplicates: true,
+  })
+
+  // Create 3 more users that own a workspace
+  let numOfWorkspaces = 3
+  for (let i = 0; i < numOfWorkspaces; i++) {
     const firstName = faker.person.firstName()
     const lastName = faker.person.lastName()
     const hashedPassword = await hash("12345678", 13)
-
-    const user = await prisma.user.create({
+    let user = await prisma.user.create({
       data: {
         image: faker.image.avatar(),
         name: `${firstName} ${lastName}`,
-        email: `${firstName.charAt(0).toLowerCase()}${lastName}@email.com`,
+        email: faker.internet.exampleEmail({ firstName, lastName }),
         hashedPassword,
       },
     })
 
-    const workspace = await prisma.workspace.create({
+    let workspace = await prisma.workspace.create({
       data: {
-        name: faker.lorem.word(8),
+        name: faker.company.name(),
         ownerId: user.id,
       },
     })
 
-    const project = await prisma.project.create({
-      data: {
-        title: faker.lorem.word(7),
-        workspaceId: workspace.id,
-        issues: {
-          createMany: {
-            data: Array.from({ length: 5 }).map(() => ({
+    // add members to workspace
+    // for (let j = 0; j < 10; j++) {
+    //   await prisma.workspaceMember.create({
+    //     data: {
+    //       userId: faker.helpers.arrayElement(users.map((user) => user.id)),
+    //       workspaceId: workspace.id,
+    //       // role: faker.helpers.arrayElement(["ADMIN", "MEMBER"]),
+    //     },
+    //   })
+    // }
+
+    let workspaceMemberData = Array.from({ length: 10 }, async () => ({
+      userId: faker.helpers.arrayElement(users.map((user) => user.id)),
+      workspaceId: workspace.id,
+    }))
+
+    let workspaceMembers = await prisma.workspaceMember.createManyAndReturn({
+      data: await Promise.all(workspaceMemberData),
+      skipDuplicates: true,
+    })
+
+    // make 2 added members admins
+    const shuffledMemberIds = faker.helpers.shuffle(
+      workspaceMembers.map((member) => member.userId),
+    )
+    const adminIds = shuffledMemberIds.slice(0, 2)
+
+    for (const adminId of adminIds) {
+      await prisma.workspaceMember.updateMany({
+        where: {
+          userId: adminId,
+          workspaceId: workspace.id,
+        },
+        data: {
+          role: "ADMIN",
+        },
+      })
+    }
+
+    // for (const adminId of adminIds) {
+    //   await prisma.workspaceMember.update({
+    //     where: {
+    //       userId_workspaceId: {
+    //         userId: adminId,
+    //         workspaceId: workspace.id,
+    //       },
+    //     },
+    //     data: {
+    //       role: "ADMIN",
+    //     },
+    //   })
+    // }
+
+    // Get all members with admin role
+    // const admins = await prisma.workspaceMember.findMany({
+    //   where: {
+    //     workspaceId: workspace.id,
+    //     role: "ADMIN",
+    //   },
+    // })
+
+    // Create 5 projects for each workspace
+    for (let i = 0; i < numOfWorkspaces; i++) {
+      const project = await prisma.project.create({
+        data: {
+          title: faker.lorem.word(7),
+          workspaceId: workspace.id,
+        },
+      })
+
+      // add workspace members to project
+      let projectMembers = await prisma.projectMember.createManyAndReturn({
+        data: await Promise.all(
+          Array.from({ length: 3 }, async () => ({
+            userId: faker.helpers.arrayElement(
+              workspaceMembers.map((user) => user.userId),
+            ),
+            projectId: project.id,
+          })),
+        ),
+        skipDuplicates: true,
+      })
+
+      // Create 5 issues for each project
+      for (let j = 0; j < 5; j++) {
+        await prisma.$transaction(async (tx) => {
+          const issue = await tx.issue.create({
+            data: {
               title: faker.hacker.phrase(),
-              description: faker.lorem.paragraph(4),
+              description: faker.lorem.paragraph(3),
               priority: faker.helpers.arrayElement([
                 "low",
                 "medium",
                 "high",
                 "no-priority",
               ]),
-              status: faker.helpers.arrayElement(["open", "closed", "in-progress"]),
-              userId: user.id,
-            })),
-          },
-        },
+              status: faker.helpers.arrayElement([
+                "open",
+                "closed",
+                "in-progress",
+                "duplicate",
+              ]),
+              ownerId: faker.helpers.arrayElement(
+                projectMembers.map((user) => user.userId),
+              ),
+              projectId: project.id,
+            },
+          })
+
+          // Shuffle the label IDs and take a random number of unique labels
+          const shuffledLabelIds = faker.helpers.shuffle(labelIds)
+          const numLabels = faker.number.int({
+            min: 0,
+            max: Math.min(4, labelIds.length),
+          })
+          const selectedLabelIds = shuffledLabelIds.slice(0, numLabels)
+
+          // Create IssueLabels
+          for (const labelId of selectedLabelIds) {
+            await tx.issueLabel.create({
+              data: {
+                issueId: issue.id,
+                labelId: labelId,
+              },
+            })
+          }
+        })
+      }
+    }
+  }
+
+  const workspaceAdmin = await prisma.workspaceMember.findFirstOrThrow({
+    where: {
+      role: "ADMIN",
+    },
+    select: {
+      workspaceId: true,
+      userId: true,
+    },
+  })
+
+  const getProject = await prisma.project.findFirstOrThrow({
+    where: {
+      workspaceId: workspaceAdmin.workspaceId,
+    },
+  })
+
+  const getProjectMember = await prisma.projectMember.findFirstOrThrow({
+    where: {
+      projectId: getProject.id,
+    },
+    select: {
+      userId: true,
+    },
+  })
+
+  const db_user = enhance(prisma, { user: { id: getProjectMember.userId } })
+
+  const issue = await prisma.issue.findFirstOrThrow({
+    where: {
+      projectId: getProject.id,
+    },
+    select: {
+      id: true,
+      ownerId: true,
+    },
+  })
+
+  if (issue && getProjectMember) {
+    // update issue status
+    const updateStatus = await db_user.issue.update({
+      select: {
+        status: true,
+        id: true,
+      },
+      where: {
+        id: issue.id,
+      },
+      data: {
+        status: faker.helpers.arrayElement([
+          "open",
+          "closed",
+          "in-progress",
+          "duplicate",
+        ]),
       },
     })
 
-    const issues = await prisma.issue.create({
+    console.log("Issue status updated:", updateStatus.status, issue.id, getProjectMember.userId)
+
+    // create status activity
+    const statusActivity = await db.statusActivity.create({
       data: {
-        title: faker.hacker.phrase(),
-        description: faker.lorem.paragraph(4),
-        priority: faker.helpers.arrayElement(["low", "medium", "high", "no-priority"]),
-        status: faker.helpers.arrayElement(["open", "closed", "in-progress"]),
-        issueLabels: {
-          createMany: {
-            data: Array.from({ length: 2 }).map(() => ({
-              labelId: faker.number.int({ min: 1, max: 7 }),
-            })),
-          },
-        },
-        userId: user.id,
-        projectId: project.id,
+        userId: getProjectMember.userId,
+        issueId: updateStatus.id,
+        name: updateStatus.status,
       },
     })
+
+    console.log("Status activity created:", statusActivity)
+  } else {
+    console.log("No issue or project member found")
   }
 }
 
