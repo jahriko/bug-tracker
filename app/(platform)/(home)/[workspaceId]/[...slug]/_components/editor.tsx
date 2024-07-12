b7c1ec662ad80417825a81c31ae6b6d481ec6b88
Jericho Opsima <jerichoopsima22@gmail.com>
2024-07-12 12:54:31 +0800

    Remove/Comment stuff

--- app/(platform)/(home)/[workspaceId]/[...slug]/_components/editor.tsx
+++ app/(platform)/(home)/[workspaceId]/[...slug]/_components/editor.tsx
@@ -1,15 +1,18 @@
 "use client" // this registers <Editor> as a Client Component
-import { BlockNoteView } from "@blocknote/ariakit"
-import "@blocknote/ariakit/style.css"
+import { BlockNoteView } from "@blocknote/mantine"
+// import "@blocknote/ariakit/style.css"
 import "@blocknote/core/fonts/inter.css"
+import "@blocknote/mantine/style.css"
 import { useCreateBlockNote } from "@blocknote/react"
-import "./styles.css"
 import { en } from "./RichTextEditor/locales/en"
+import "./styles.css"
 // Our <Editor> component we can reuse later
 export default function Editor() {
   // Creates a new editor instance.
   const editor = useCreateBlockNote({
+    // defaultStyles: false,
     dictionary: en,
+
     domAttributes: {
       inlineContent: {
         class: "!prose !prose-sm dark:prose-invert !max-w-none ",
@@ -21,6 +24,7 @@ export default function Editor() {
   return (
     <BlockNoteView
       data-theming-css-variables-demo
+      // className="max-w-[1500px]"
       editor={editor}
       sideMenu={false}
       slashMenu={false}
