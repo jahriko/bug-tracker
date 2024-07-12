114bac984010219446da57aff808d80acf385352
Jericho Opsima <jerichoopsima22@gmail.com>
2024-07-12 08:05:39 +0800

    Custom en locale for richtext editor

--- app/(platform)/(home)/[workspaceId]/[...slug]/_components/editor.tsx
+++ app/(platform)/(home)/[workspaceId]/[...slug]/_components/editor.tsx
@@ -4,23 +4,15 @@ import "@blocknote/ariakit/style.css"
 import "@blocknote/core/fonts/inter.css"
 import { useCreateBlockNote } from "@blocknote/react"
 import "./styles.css"
+import { en } from "./RichTextEditor/locales/en"
 // Our <Editor> component we can reuse later
 export default function Editor() {
   // Creates a new editor instance.
   const editor = useCreateBlockNote({
-    dictionary: {
-      placeholders: {
-        default: "Enter description...",
-        heading: "Heading",
-        bulletListItem: "List",
-        numberedListItem: "List",
-        checkListItem: "List",
-      },
-    },
+    dictionary: en,
   })
 
   console.log(editor.document)
-  //   editor.removeBlocks(["SideMenu"])
 
   // Renders the editor instance using a React component.
   return (
