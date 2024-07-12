eb6f169d792847d2d5f1bf6a109fc6e03c8b06c2
Jericho Opsima <jerichoopsima22@gmail.com>
2024-07-12 19:03:12 +0800

    Include common toolbars only

--- app/(platform)/(home)/[workspaceId]/[...slug]/_components/editor.tsx
+++ app/(platform)/(home)/[workspaceId]/[...slug]/_components/editor.tsx
@@ -1,17 +1,34 @@
 "use client" // this registers <Editor> as a Client Component
 import { BlockNoteView } from "@blocknote/mantine"
 // import "@blocknote/ariakit/style.css"
+import { locales } from "@blocknote/core"
 import "@blocknote/core/fonts/inter.css"
 import "@blocknote/mantine/style.css"
-import { useCreateBlockNote } from "@blocknote/react"
-import { en } from "./RichTextEditor/locales/en"
+import {
+  BasicTextStyleButton,
+  BlockTypeSelect,
+  CreateLinkButton,
+  FileCaptionButton,
+  FileReplaceButton,
+  FormattingToolbar,
+  FormattingToolbarController,
+  useCreateBlockNote,
+} from "@blocknote/react"
 import "./styles.css"
+
 // Our <Editor> component we can reuse later
 export default function Editor() {
   // Creates a new editor instance.
   const editor = useCreateBlockNote({
+    trailingBlock: false,
     // defaultStyles: false,
-    dictionary: en,
+    dictionary: {
+      ...locales.en,
+      placeholders: {
+        ...locales.en.placeholders,
+        default: "Enter description..." || locales.en.placeholders.default,
+      },
+    },
 
     domAttributes: {
       blockContent: {
@@ -24,11 +41,32 @@ export default function Editor() {
   return (
     <BlockNoteView
       data-theming-css-variables-demo
-      // className="max-w-[1500px]"
       editor={editor}
+      formattingToolbar={false}
       sideMenu={false}
-      slashMenu={false}
+      // slashMenu={false}
+      spellCheck="false"
       theme="light"
-    />
+    >
+      <FormattingToolbarController
+        formattingToolbar={() => (
+          <FormattingToolbar>
+            <BlockTypeSelect key="blockTypeSelect" />
+
+            <FileCaptionButton key="fileCaptionButton" />
+            <FileReplaceButton key="replaceFileButton" />
+
+            <BasicTextStyleButton basicTextStyle="bold" key="boldStyleButton" />
+            <BasicTextStyleButton basicTextStyle="italic" key="italicStyleButton" />
+            <BasicTextStyleButton basicTextStyle="underline" key="underlineStyleButton" />
+            <BasicTextStyleButton basicTextStyle="strike" key="strikeStyleButton" />
+            {/* Extra button to toggle code styles */}
+            <BasicTextStyleButton basicTextStyle="code" key="codeStyleButton" />
+
+            <CreateLinkButton key="createLinkButton" />
+          </FormattingToolbar>
+        )}
+      />
+    </BlockNoteView>
   )
 }
