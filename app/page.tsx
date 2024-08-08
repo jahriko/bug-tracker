1b4b38f84ff653c9735f100c352648d949dbd5fa
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-08 18:02:26 +0800

    Fix RSC issue

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,7 @@
-import { getCurrentUser } from "@/lib/get-current-user"
-
-export default async function LandingPage() {
-  const user = await getCurrentUser()
-
+export default function LandingPage() {
   return (
     <div>
       <h1>Welcome to the landing page</h1>
-      {user ? <p>Welcome, {user.name}</p> : <p>Not logged in</p>}
     </div>
   )
 }
