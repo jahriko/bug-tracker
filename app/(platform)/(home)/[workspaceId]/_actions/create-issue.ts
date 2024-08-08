95044a69256b98d515a85eae9cd2771f05d9a0cd
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-08 17:44:04 +0800

    Refactor actions in issue

--- app/(platform)/(home)/[workspaceId]/_actions/create-issue.ts
+++ app/(platform)/(home)/[workspaceId]/_actions/create-issue.ts
@@ -1,47 +1,38 @@
 "use server"
 
-import { getCurrentUser } from "@/lib/get-current-user"
 import { getPrisma } from "@/lib/getPrisma"
+import { authActionClient } from "@/lib/safe-action"
 import { IssueSchema } from "@/lib/validations"
 import { revalidateTag } from "next/cache"
 
-export async function createIssue(data: IssueSchema) {
-  const { title, description, status, priority, labels, projectId, assigneeId } =
-    IssueSchema.parse(data)
-
-  try {
-    const user = await getCurrentUser()
-
-    if (!user) {
-      throw new Error("User not authenticated.")
-    }
-
-    await getPrisma(user.userId).issue.create({
-      data: {
-        title,
-        description,
-        status,
-        priority,
-        assigneeId: assigneeId ?? null,
-        projectId,
-        userId: user.userId,
-        issueLabels: {
-          create: [...labels.map((label) => ({ labelId: label.id }))],
+export const createIssue = authActionClient
+  .schema(IssueSchema)
+  .action(
+    async ({
+      parsedInput: { title, description, status, priority, assigneeId, labels, projectId },
+      ctx: { userId },
+    }) => {
+      await getPrisma(userId).issue.create({
+        data: {
+          title,
+          description,
+          status,
+          priority,
+          assignedUserId: assigneeId ?? null,
+          projectId,
+          ownerId: userId,
+          ...(labels && labels.length > 0
+            ? {
+                labels: {
+                  create: labels.map((label) => ({ labelId: label.id })),
+                },
+              }
+            : {}),
         },
-      },
-    })
+      })
 
-    revalidateTag("issue-list")
+      revalidateTag("issue-list")
 
-    return {
-      code: "success",
-      message: "Issue created successfully",
-    }
-  } catch (error: unknown) {
-    console.error("Error creating Issue: ", error)
-    return {
-      code: "error",
-      message: "Error creating an issue",
-    }
-  }
-}
+      return { success: true }
+    },
+  )
