9d07efdd384afb830b42fc8b8b048d890f3bb1bd
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-08 17:49:07 +0800

    Fix navbar link

--- app/(platform)/(home)/_components/NavbarLinks.tsx
+++ app/(platform)/(home)/_components/NavbarLinks.tsx
@@ -4,13 +4,16 @@ import { NavbarItem } from "@/components/catalyst/navbar"
 import { usePathname } from "next/navigation"
 
 export default function NavbarLinks() {
-  const path = usePathname().split("/")[2]
+  const pathname = usePathname()
+  const segments = pathname.split("/")
+  const workspaceSlug = segments[1] // Assuming workspace slug is the first segment
+
   return (
     <>
-      <NavbarItem current={path === "issues"} href="issues/" key="issues">
+      <NavbarItem current={segments[2] === "issues"} href={`/${workspaceSlug}/issues`} key="issues">
         Issues
       </NavbarItem>
-      <NavbarItem current={path === "projects"} href="projects/" key="projects">
+      <NavbarItem current={segments[2] === "projects"} href={`/${workspaceSlug}/projects`} key="projects">
         Projects
       </NavbarItem>
     </>
