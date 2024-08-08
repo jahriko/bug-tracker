385dacbd3b17414b57a885ff1e0564bf3add83cc
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-08 19:32:56 +0800

    Update action

--- app/(platform)/(home)/[workspaceId]/create-project/create-project.ts
+++ app/(platform)/(home)/[workspaceId]/create-project/create-project.ts
@@ -1,22 +1,19 @@
 "use server"
-import prisma from "@/lib/prisma"
+import { getPrisma } from "@/lib/getPrisma"
 import { authActionClient } from "@/lib/safe-action"
-import { enhance } from "@zenstackhq/runtime"
 import { z } from "zod"
 
 const schema = z.object({
-  id: z.string().min(2, { message: "ID is required." }),
+  identifier: z.string().min(2, { message: "ID is required." }),
   title: z.string().min(2, { message: "Project name is required." }),
 })
 
 export const createProject = authActionClient
   .schema(schema)
-  .action(async ({ parsedInput: { title, id }, ctx: { userId } }) => {
-    const authZ = enhance(prisma, { user: { id: userId } })
-
-    await authZ.project.create({
+  .action(async ({ parsedInput: { title, identifier }, ctx: { userId } }) => {
+    await getPrisma(userId).project.create({
       data: {
-        id,
+        identifier,
         title,
       },
     })
