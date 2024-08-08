38ecf5871c8b5673f63e08cb08216d2f2e71cc6e
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-08 17:56:53 +0800

    Update switch workspace component

--- app/(platform)/(home)/_components/switch-workspace.tsx
+++ app/(platform)/(home)/_components/switch-workspace.tsx
@@ -10,40 +10,36 @@ import {
   DropdownMenu,
 } from "@/components/catalyst/dropdown"
 import { NavbarItem, NavbarLabel } from "@/components/catalyst/navbar"
-import { ChevronDownIcon, PlusIcon } from "@heroicons/react/16/solid"
+import { ChevronDownIcon, Cog6ToothIcon, PlusIcon } from "@heroicons/react/16/solid"
 import { Workspace } from "@prisma/client"
 import { usePathname } from "next/navigation"
 
 export default function SwitchWorkspace({ workspaces }: { workspaces: Workspace[] }) {
   const path = usePathname()
   const getWorkspaceUrl = path.split("/")[1]
-  const workspaceUrlToName = workspaces.find(
-    (workspace) => workspace.url === getWorkspaceUrl,
-  )?.name
+  const workspaceUrlToName = workspaces.find((workspace) => workspace.url === getWorkspaceUrl)?.name
+  // const [isOpen, setIsOpen] = useState(false)
+
   return (
     <Dropdown>
       <DropdownButton as={NavbarItem} className="max-lg:hidden">
-        <Avatar
-          className="bg-zinc-500 text-white"
-          initials={workspaceUrlToName?.substring(0, 2)}
-          slot="icon"
-        />
+        <Avatar initials={workspaceUrlToName?.substring(0, 2) ?? ""} slot="icon" />
         <NavbarLabel>{workspaceUrlToName}</NavbarLabel>
         <ChevronDownIcon />
       </DropdownButton>
       <DropdownMenu anchor="bottom start" className="min-w-80 lg:min-w-64">
         {workspaces.map((workspace) => (
-          <DropdownItem href={`/${workspace.url}/issues`} key={workspace.id}>
-            <Avatar
-              className="bg-zinc-500 text-white"
-              initials={workspace.name.substring(0, 2)}
-              slot="icon"
-            />
+          <DropdownItem href={`/${workspace.url}`} key={workspace.id}>
+            <Avatar initials={workspace.name.substring(0, 2)} slot="icon" />
             <DropdownLabel>{workspace.name}</DropdownLabel>
           </DropdownItem>
         ))}
         <DropdownDivider />
-        <DropdownItem href="/create-workspace">
+        <DropdownItem href={`/${getWorkspaceUrl}/settings`}>
+          <Cog6ToothIcon />
+          <DropdownLabel>Workspace Settings</DropdownLabel>
+        </DropdownItem>
+        <DropdownItem href="/create-workspace/">
           <PlusIcon />
           <DropdownLabel>New workspace&hellip;</DropdownLabel>
         </DropdownItem>
