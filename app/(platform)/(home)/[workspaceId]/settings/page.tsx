114e16d334dcb2917287b40d3b41e6e98137b0f5
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-15 21:02:17 +0800

    Update settings page

--- app/(platform)/(home)/[workspaceId]/settings/page.tsx
+++ app/(platform)/(home)/[workspaceId]/settings/page.tsx
@@ -1,6 +1,9 @@
+import { Button } from "@/components/catalyst/button"
 import { getCurrentUser } from "@/lib/get-current-user"
 import { getPrisma } from "@/lib/getPrisma"
+import { LockClosedIcon, LockOpenIcon, PencilIcon } from "@heroicons/react/24/outline"
 import { notFound } from "next/navigation"
+
 export default async function SettingsPage({ params }: { params: { workspaceId: string } }) {
   const { workspaceId } = params
 
@@ -28,9 +31,10 @@ export default async function SettingsPage({ params }: { params: { workspaceId: 
           <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Workspace name</dt>
           <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
             <div className="text-gray-900">{workspace.name}</div>
-            <button className="font-semibold text-indigo-600 hover:text-indigo-500" type="button">
+            <Button plain>
+              <PencilIcon className="size-4" />
               Update
-            </button>
+            </Button>
           </dd>
         </div>
       </dl>
@@ -39,9 +43,31 @@ export default async function SettingsPage({ params }: { params: { workspaceId: 
           <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">URL</dt>
           <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
             <div className="text-gray-900">{workspace.url}</div>
-            <button className="font-semibold text-indigo-600 hover:text-indigo-500" type="button">
+            <Button plain>
+              <PencilIcon className="size-4" />
               Update
-            </button>
+            </Button>
+          </dd>
+        </div>
+      </dl>
+      <dl className="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
+        <div className="pt-6 sm:flex">
+          <dt className="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">Project Visibility</dt>
+          <dd className="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
+            <div className="text-gray-900">{workspace.private ? "Private" : "Public"}</div>
+            <Button plain>
+              {workspace.private ? (
+                <>
+                  <LockClosedIcon className="size-4" />
+                  Make Private
+                </>
+              ) : (
+                <>
+                  <LockOpenIcon className="size-4" />
+                  Make Public
+                </>
+              )}
+            </Button>
           </dd>
         </div>
       </dl>
