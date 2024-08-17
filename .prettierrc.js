902c9c26918c8e08c5fc03895b1164e3c436e83e
Jericho Opsima <jerichoopsima22@gmail.com>
2024-08-17 18:16:19 +0800

    Prettier config changes

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,9 +3,6 @@ import vercelPrettierOptions from '@vercel/style-guide/prettier';
 /** @type {import('prettier').Config} */
 const config = {
   ...vercelPrettierOptions,
-  tabWidth: 2,
-  singleQuote: false,
-  jsxSingleQuote: false,
   plugins: [...vercelPrettierOptions.plugins, 'prettier-plugin-prisma'],
 };
 
